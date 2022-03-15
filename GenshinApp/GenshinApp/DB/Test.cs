92a45d53c43619f1b813f3ab380cb50dd37686ad
kzlp-ldz <kamilyazakirova15935700@gmail.com>
2022-03-15 13:47:48 +0300

    checking nuget packet

---
+++ GenshinApp/GenshinApp/DB/Test.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using GenshinInfo;
+using GenshinInfo.Managers;
+using GenshinInfo.Models;
+using GenshinInfo.Enums;
+
+namespace GenshinApp.DB
+{
+    internal class Test
+    {
+        private string value;
+        private GachaInfoManager manag;
+
+        public Test()
+        {
+            manag = new GachaInfoManager();
+        }
+        //To take a key from this long link
+        public void SetKey(string key)
+        {
+            value = key;
+            GachaInfoManager gacha = new GachaInfoManager();
+            gacha.SetAuthKeyByUrl(value);
+        }
+        //Event character banner
+        public async Task<List<GachaDataInfo>> CheckCharacterEventWishes()
+        {
+            return await manag.GetGachaInfos(GachaTypeNum.CharacterEvent);
+        }
+        //Event weapon banner
+        public async Task<List<GachaDataInfo>> CheckWeaponEventWishes()
+        {
+            return await manag.GetGachaInfos(GachaTypeNum.WeaponEvent);
+        }
+        //Novice banner
+        public async Task<List<GachaDataInfo>> CheckNoviceWishes()
+        {
+            return await manag.GetGachaInfos(GachaTypeNum.Novice);
+        }
+        //Standart banner
+        public async Task<List<GachaDataInfo>> CheckStandertWishes()
+        {
+            return await manag.GetGachaInfos(GachaTypeNum.Permanent);
+        }
+    }
+}
